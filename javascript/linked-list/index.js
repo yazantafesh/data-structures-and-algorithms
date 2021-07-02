ccafd0a8eb1fb2fd05f6a42eff829fd6364d37e4
yazantafesh <yazantafesh1@gmail.com>
2021-07-02 16:44:18 +0300

    done

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,67 @@
 'use strict';
 
-class LinkedList {
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
 
+class LinkedList {
+  constructor() {
+      this.head = null;
+  }
+
+  insert(value) {
+    let node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
+    }
+  }
+
+  includes(value) {
+    let pointer = this.head;
+    if (!this.head) {
+      return false;
+    } else {
+
+      while (pointer.next) {
+        if (pointer.value == value) {
+          return true;
+        }
+        pointer = pointer.next;
+
+        if (pointer.next == null && value == pointer.value) {
+          return true;
+        }
+      }
+      return false;
+    }
+  }
+
+  toString() {
+    let string = '';
+    let pointer = this.head;
+
+    while (pointer.next) {
+      if(pointer.value == null || pointer.value == undefined){
+        string = string + `NULL -> `;
+      }else{
+
+        string = string + `{${pointer.value}} -> `
+      }
+      pointer = pointer.next;
+      if(pointer.next == null){
+        string = string + `{${pointer.value}}`
+      }
+    }
+
+    return string;
+
+  }
 }
 
 module.exports = LinkedList;
